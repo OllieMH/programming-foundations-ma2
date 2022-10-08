c8b0966dda75bcf90777c0b51e9002cba0d1fe11
OllieMH <olivermh99@hotmail.com>
2022-10-08 12:20:38 +0200

    Finished the assignment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// question 1
+
 var pets = [
     {
       type: "cat",
@@ -20,4 +22,26 @@ for (var i = 0; i < pets.length; i++) {
     if (age >= 4) {
         console.log(type);
     }
+};
+
+// question 2
+
+function checkBool(inputValue){
+  if (typeof inputValue === "boolean") {
+    console.log(inputValue);
+  }
+  else {
+    console.log("Please pass a boolean value in");
+  }
+};
+checkBool(true);
+
+// question 3
+
+var button = document.querySelector("button");
+var h2 = document.querySelector("h2");
+
+button.onclick = function () {
+  h2.style.color = "blue";
+  h2.innerHTML = "Updated subheading"
 };
